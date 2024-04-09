87221b49261a6a6f801d23f92f4ca75cc7aea37f
reganst93 <stmi.93@gmail.com>
2024-04-08 20:50:30 -0400

    primera modificacion views

---
+++ mascotasApp/mascotasApp/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for mascotasApp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from ejemplo.views import ejemplo, gato, about, contact
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("ejemplo/", ejemplo),
+    path("", gato),
+    path("about/", about),
+    path("contact/", contact),
+]
